da747c3d41247f0b1d55b895fb0689ea88ab9177
Yota Toyama <raviqqe@gmail.com>
2025-12-14 23:52:39 -0800

    Fix

--- tools/example_test.sh
+++ tools/example_test.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+duration=20
+
 cd $(dirname $0)
 
 for directory in $(ls examples); do
@@ -9,7 +11,7 @@ for directory in $(ls examples); do
     cd examples/$directory
     cargo run &
     pid=$!
-    sleep 20
+    sleep $duration
     kill $pid
   )
 done
